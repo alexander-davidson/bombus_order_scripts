d6a82a4f3981ae6c4a94c3a3e04f01bc8dedcb77
alexander-davidson <bty571@qmul.ac.uk>
2025-07-02 11:39:42 +0100

    updated python frame extractor

--- frame_extractor_qr_recognition/live_feed_frame_extractor.py
+++ frame_extractor_qr_recognition/live_feed_frame_extractor.py
@@ -1,9 +1,11 @@
 import os
 import cv2
+import time
 from datetime import datetime
 
 # path = r'D:\ALEX\2025\pyprojects\get_frames\frames'
-path = r'D:\Order_Discrimination_Bombus\live_frame_storage' # 2.07 PC
+# path = r'D:\Order_Discrimination_Bombus\live_frame_storage' # 2.07 PC
+path = r'/Users/alexander/Documents/test_april_2005_delete_May/frame'
 
 # checking git from onedrive
 
@@ -16,40 +18,46 @@ video = cv2.VideoCapture(0)
 #video.set(cv2.CAP_PROP_FRAME_WIDTH, 1920/2)
 #video.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080/2)
 
+try:
+    while True:
+        ret, img = video.read()
+        # print(img.shape)
+        # font = cv2.FONT_HERSHEY_PLAIN
+        #cv2.putText(img, str(datetime.now()), (20, 40),
+        #            font, 2, (255, 255, 255), 2, cv2.LINE_AA)
 
-while True:
-    ret, img = video.read()
-    #print(img.shape)
-    font = cv2.FONT_HERSHEY_PLAIN
-    #cv2.putText(img, str(datetime.now()), (20, 40),
-    #            font, 2, (255, 255, 255), 2, cv2.LINE_AA)
+        # Display the image
+        #display_size = cv2.resize(img, (320, 240))
+        #cv2.imshow('live video', display_size)
 
-    # Display the image
-    display_size = cv2.resize(img, (320, 240))
-    cv2.imshow('live video', display_size)
+        # wait for user to press any key
+        #key = cv2.waitKey(100)
 
-    # wait for user to press any key
-    key = cv2.waitKey(100)
-
-    # wait variable is to calculate waiting time
-    wait = wait + 1
-    print(wait)
-    if key == ord('q'):
-        break
-    # when it reaches to 5000 milliseconds
-    # we will save that frame in given folder
-    if wait == 5:
-        filename = 'Frame_' + str(i) + '.jpg'
-
-        # Save the images in given path
+        # wait variable is to calculate waiting time
+        # wait = wait + 1
+        # print(wait)
+        # if key == ord('q'):
+        #     break
+        # when it reaches to 5000 milliseconds
+        # we will save that frame in given folder
+        # if wait >= 5:
+        #     filename = 'Frame_' + str(i) + '.jpg'
+        #
+        #     # Save the images in given path
+        #     cv2.imwrite(filename, img)
+        #     #i = i + 1
+        #     wait = 0
+        #if i >= 5:
+            #i = 1
+        filename = 'Frame.jpg'
         cv2.imwrite(filename, img)
-        #i = i + 1
-        wait = 0
-    #if i >= 5:
-        #i = 1
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    print("ended")
 
-# close the camera
-video.release()
+finally:
+    # close the camera
+    video.release()
 
-# close open windows
-cv2.destroyAllWindows()
+    # close open windows
+    # cv2.destroyAllWindows()

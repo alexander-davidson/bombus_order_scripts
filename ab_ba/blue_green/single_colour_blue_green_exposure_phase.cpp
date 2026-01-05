fc07ccd0962868bb2f968d148c82d415ea64dc0a
alexander-davidson <167880750+alexander-davidson@users.noreply.github.com>
2026-01-05 12:46:40 +0000

    Add files via upload
    The single blue vs green (i.e. A vs B) for the exposure phase, to be uploaded to rotators for the exposure arena.

---
+++ ab_ba/blue_green/single_colour_blue_green_exposure_phase.cpp
@@ -0,0 +1,51 @@
+// library for neopixel LEDs
+#include <Adafruit_NeoPixel.h>
+
+// specifies AVR microcontroller (arduino, as opposed to other models with different microcontrollers)
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+// which pin is outputs to LEDs
+#define PIN 12
+
+// define how many rings there are
+int number_of_rings = 2;
+
+// define how many LEDs there are in each
+int number_of_leds = 7;
+
+// define duration for colours
+int col_duration_val = 500;
+// define duration for off state
+int del_val = 2500; 
+
+int intensity = 255;
+
+// number of pixels (i.e. LEDs in ring, put number of LEDs in array times number arrays)
+#define NUMPIXELS number_of_leds* number_of_rings
+
+// parameters are number of pixels, which pin, pixel type (Neo, Green, Red, Blue), kHz datastream (800 kHz)
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pixels.begin();
+}
+
+void loop() {
+  pixels.fill(pixels.Color(0, 0, intensity), 0, 7); // rgb, starting led, how many leds from starting led
+  pixels.fill(pixels.Color(0, intensity, 0), 7, 7);
+  pixels.show();
+  delay(col_duration_val);
+
+  //pixels.fill(pixels.Color(0, intensity, 0, 0), 0, 7);
+  //pixels.fill(pixels.Color(0, 0, 0, intensity), 7, 7);
+  //pixels.show();
+  //delay(col_duration_val);
+
+  pixels.clear();
+  pixels.show();
+  delay(del_val);
+}
